21d72833d303fe3c78b58dec5859e9c0ffb08627
Pravin Pushkar <pravin.pratap.pushkar@sap.com>
2020-10-02 19:38:16 +0530

    Inital project setup from spring initializer

---
+++ src/main/java/com/fuber/fuberoncalltaxiservice/FuberOnCallTaxiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fuber.fuberoncalltaxiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuberOnCallTaxiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuberOnCallTaxiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fuber/fuberoncalltaxiservice/FuberOnCallTaxiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fuber.fuberoncalltaxiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuberOnCallTaxiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
